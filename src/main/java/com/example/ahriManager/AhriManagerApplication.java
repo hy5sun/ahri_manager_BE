9847f91973cc87fa001761cb8437c7a9cc2d65f0
hy5sun <ssunykkim@naver.com>
2025-01-05 03:26:03 +0900

    initial commit

---
+++ src/main/java/com/example/ahriManager/AhriManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ahriManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhriManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhriManagerApplication.class, args);
+	}
+
+}

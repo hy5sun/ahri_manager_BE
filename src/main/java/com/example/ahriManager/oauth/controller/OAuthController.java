3b4a011618aa1e65305d44b555291b6d35cc489f
hy5sun <ssunykkim@naver.com>
2025-01-05 03:39:36 +0900

    feat: OAuth Service, Controller 생성

---
+++ src/main/java/com/example/ahriManager/oauth/controller/OAuthController.java
@@ -0,0 +1,13 @@
+package com.example.ahriManager.oauth.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/oauth")
+@RequiredArgsConstructor
+@Slf4j
+public class OAuthController {
+}

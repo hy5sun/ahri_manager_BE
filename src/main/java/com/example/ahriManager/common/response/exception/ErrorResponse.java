9ab1bd03161440e7614fbb4bfc3166679e59e40c
hy5sun <ssunykkim@naver.com>
2025-01-05 04:50:59 +0900

    feat: Error Response 생성

---
+++ src/main/java/com/example/ahriManager/common/response/exception/ErrorResponse.java
@@ -0,0 +1,74 @@
+package com.example.ahriManager.common.response.exception;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.BindingResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor
+public class ErrorResponse {
+    private int statusCode;
+    private String error;
+    private String message;
+    private List<FieldError> errors;
+
+    private ErrorResponse(final ErrorCode code) {
+        this.statusCode = code.getStatusCode();
+        this.error = code.getError();
+        this.message = code.getMessage();
+        this.errors = new ArrayList<>();
+    }
+
+    private ErrorResponse(final ErrorCode code, final List<FieldError> errors) {
+        this.statusCode = code.getStatusCode();
+        this.error = code.getError();
+        this.message = code.getMessage();
+        this.errors = errors;
+    }
+
+    public static ErrorResponse of(final ErrorCode code) {
+        return new ErrorResponse(code);
+    }
+
+    public static ErrorResponse of(final ErrorCode code, final List<FieldError> errors) {
+        return new ErrorResponse(code, errors);
+    }
+
+    public static ErrorResponse of(final ErrorCode code, final BindingResult bindingResult) {
+        return new ErrorResponse(code, FieldError.of(bindingResult));
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class FieldError {
+        private String field;
+        private String value;
+        private String reason;
+
+        private FieldError(String field, String value, String reason) {
+            this.field = field;
+            this.value = value;
+            this.reason = reason;
+        }
+
+        public static List<FieldError> of(final String field, final String value, final String reason) {
+            List<FieldError> fieldErrors = new ArrayList<>();
+            fieldErrors.add(new FieldError(field, value, reason));
+            return fieldErrors;
+        }
+
+        private static List<FieldError> of(final BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()))
+                    .collect(Collectors.toList());
+        }
+    }
+}

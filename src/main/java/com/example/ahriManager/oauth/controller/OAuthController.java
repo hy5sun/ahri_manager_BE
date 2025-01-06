0179c7d9f89bd63d2e13051d8ece174acb122721
hy5sun <ssunykkim@naver.com>
2025-01-06 15:10:17 +0900

    feat: 카카오 로그인 요청 리다이렉트 API 생성

--- src/main/java/com/example/ahriManager/oauth/controller/OAuthController.java
+++ src/main/java/com/example/ahriManager/oauth/controller/OAuthController.java
@@ -1,13 +1,33 @@
 package com.example.ahriManager.oauth.controller;
 
+import com.example.ahriManager.common.exception.BusinessException;
+import com.example.ahriManager.oauth.service.OAuthService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+import static com.example.ahriManager.common.exception.type.ErrorCode.INTERNAL_SERVER_ERROR;
 
 @RestController
 @RequestMapping("/oauth")
 @RequiredArgsConstructor
 @Slf4j
 public class OAuthController {
+
+    private final OAuthService oAuthService;
+
+    @GetMapping("/kakao/login")
+    @ResponseStatus(HttpStatus.FOUND)
+    public void connectLogin(HttpServletResponse response) {
+        String url = oAuthService.getKakaoLoginUrl();
+        try {
+            response.sendRedirect(url);
+        } catch (IOException e) {
+            throw new BusinessException(INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/com/example/ahriManager/oauth/service/OAuthService.java
+++ src/main/java/com/example/ahriManager/oauth/service/OAuthService.java
@@ -1,9 +1,23 @@
 package com.example.ahriManager.oauth.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class OAuthService {
+    private final static String KAKAO_AUTH_BASE_URL = "https://kauth.kakao.com";
+    private final static String CODE = "code";
+
+    @Value("${oauth.kakao.clientId}")
+    private String kakaoClientId;
+
+    @Value("${oauth.kakao.redirectURI}")
+    private String kakaoRedirectURI;
+
+    public String getKakaoLoginUrl() {
+        return  KAKAO_AUTH_BASE_URL + "/oauth/authorize?client_id=" + kakaoClientId
+                + "&redirect_uri=" + kakaoRedirectURI + "&response_type=" + CODE;
+    }
 }
